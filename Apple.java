503203c54bfff0f6007955a254a256b2c4465875
Froyder <78364851+Froyder@users.noreply.github.com>
2021-03-02 16:42:10 +0300

    Update Apple.java

--- Apple.java
+++ Apple.java
@@ -1,15 +1,8 @@
 package ru.geek.homeworks.lesson11.generics.fruitsandboxes;
-
 public class Apple extends Fruit {
 
-    private static final double WEIGHT = 1.0;
-
-    public Apple () {
-        super(1.0);
-    }
-
-    public static double getFruitWeight() {
-        return WEIGHT;
+    public Apple (double weight) {
+        super(weight);
     }
 
     @Override
