d7021f4bdc2665df243c8f0bc87177a517911c03
Froyder <78364851+Froyder@users.noreply.github.com>
2021-03-02 16:42:37 +0300

    Update Orange.java

--- Orange.java
+++ Orange.java
@@ -2,14 +2,8 @@ package ru.geek.homeworks.lesson11.generics.fruitsandboxes;
 
 public class Orange extends Fruit {
 
-    private static final double WEIGHT = 1.5;
-
-    public Orange () {
-        super(1.5);
-    }
-
-    public static double getFruitWeight() {
-        return WEIGHT;
+    public Orange (double weight) {
+        super(weight);
     }
 
     @Override
